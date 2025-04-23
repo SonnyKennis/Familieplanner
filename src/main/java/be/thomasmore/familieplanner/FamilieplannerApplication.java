f441a4a4cbccb50c061c991546cfe99e6683ade9
Sonny <151723053+SonnyKennis@users.noreply.github.com>
2025-04-23 15:36:56 +0200

    Start project

---
+++ src/main/java/be/thomasmore/familieplanner/FamilieplannerApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.familieplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilieplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilieplannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/familieplanner/FamilieplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.familieplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilieplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
