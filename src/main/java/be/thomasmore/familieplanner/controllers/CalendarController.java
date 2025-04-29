39b137e1e36874631dda6632682833d077fcbc3c
Sonny <151723053+SonnyKennis@users.noreply.github.com>
2025-04-29 18:08:36 +0200

    tasks en activeiten overzicht

--- src/main/java/be/thomasmore/familieplanner/controllers/CalendarController.java
+++ src/main/java/be/thomasmore/familieplanner/controllers/CalendarController.java
@@ -1,6 +1,7 @@
 package be.thomasmore.familieplanner.controllers;
 import be.thomasmore.familieplanner.model.Activity;
 import be.thomasmore.familieplanner.repository.ActivityRepository;
+import be.thomasmore.familieplanner.repository.TasksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +15,15 @@ public class CalendarController {
     @Autowired
     private ActivityRepository activiteitRepository;
 
+    @Autowired
+    private TasksRepository tasksRepository;
+
 
     @GetMapping("/calendaroverview")
     public String calendarOverview(Model model) {
         Iterable<Activity> activities = activiteitRepository.findAll();
         model.addAttribute("activiteiten", activities);
+        model.addAttribute("tasks", tasksRepository.findAll());
         return "calendaroverview";
     }
 
