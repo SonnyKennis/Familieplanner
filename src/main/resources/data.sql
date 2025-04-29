0c14ee5973249081c9b6e5b73448ec2b8ac855d4
Sonny <151723053+SonnyKennis@users.noreply.github.com>
2025-04-29 16:13:00 +0200

    taken data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,12 @@ VALUES ('🎮 Speeltijd', 'Vrije tijd om spelletjes te spelen', '2025-04-28T09:3
        ('⚽ Voetbaltraining', 'Teamtraining op het veld', '2025-04-28T16:00:00', '2025-04-28T17:30:00'),
        ('📚 Huiswerkbegeleiding', 'Hulp bij schoolopdrachten', '2025-04-28T18:00:00', '2025-04-28T19:00:00'),
        ('🎨 Tekenen & Schilderen', 'Creatief tekenen en schilderen', '2025-04-29T11:00:00', '2025-04-29T12:00:00'),
-       ('🏞️ Familiepicknick', 'Picknick in de buitenlucht met het hele gezin', '2025-04-30T12:00:00', '2025-04-30T15:00:00');
+       ('🏞️ Familiepicknick', 'Picknick in de buitenlucht met het hele gezin', '2025-04-30T12:00:00',
+        '2025-04-30T15:00:00');
+
+INSERT INTO tasks (titel, beschrijving, starttijd, eindtijd)
+VALUES ('Boodschappen doen', 'Koop groenten, fruit en brood.', '2025-04-30 10:00:00', '2025-04-30 11:00:00'),
+       ('Huiswerk begeleiden', 'Help kinderen met wiskunde.', '2025-04-30 15:30:00', '2025-04-30 16:30:00'),
+       ('Familiediner plannen', 'Bespreek menu en genodigden.', '2025-05-01 18:00:00', '2025-05-01 19:30:00'),
+       ('Tuin opruimen', 'Verwijder onkruid en maai het gras.', '2025-05-02 09:00:00', '2025-05-02 10:30:00'),
+       ('Schoolproject afmaken', 'Maak PowerPoint-presentatie af.', '2025-05-03 13:00:00', '2025-05-03 15:00:00');
