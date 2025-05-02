b98408b7ea3c0650a5f7ac5f131fba0f19c51ac6
Jasper Neyens <Jasper.Neyens@student.ucll.be>
2025-05-02 10:08:03 +0200

    repository boodschap aangemaakt

---
+++ src/main/java/be/thomasmore/familieplanner/repository/BoodschapRepository.java
@@ -0,0 +1,7 @@
+package be.thomasmore.familieplanner.repository;
+
+import be.thomasmore.familieplanner.model.Boodschap;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BoodschapRepository extends CrudRepository<Boodschap, Integer> {
+}
