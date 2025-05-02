8583329a060f67b5550639407e45e5aca729b6d8
Jasper Neyens <Jasper.Neyens@student.ucll.be>
2025-05-02 12:27:57 +0200

    Boodschappencontroller werkt en de html is in orde

--- src/main/java/be/thomasmore/familieplanner/controllers/BoodschapController.java
+++ src/main/java/be/thomasmore/familieplanner/controllers/BoodschapController.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class BoodschapController {
 
@@ -17,6 +20,14 @@ public class BoodschapController {
     public String boodschappenlijst(Model model) {
         Iterable<Boodschap> boodschappen = boodschapRepository.findAll();
         model.addAttribute("boodschappen", boodschappen);
-        return "boodschap";
+        model.addAttribute("nieuweBoodschap", new Boodschap());
+        return "boodschappenlijst";
+    }
+
+    @PostMapping("/boodschappenlijst")
+    public String addBoodschap(@ModelAttribute("nieuweBoodschap") Boodschap boodschap) {
+        boodschapRepository.save(boodschap);
+        return "redirect:/boodschappenlijst";
     }
 }
+
